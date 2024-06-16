4ca5ebf5fb9bb5e4e0a3d39798b697efadfa8b88
maureen <maureen.murimi@moringaschool.com>
2024-06-16 19:23:40 +0300

    Defined Order class with SQLAlchemy model inheritance.

--- models.py
+++ models.py
@@ -44,3 +44,16 @@ class User(db.Model):
 
     def authenticate(self, pwd):
         return bcrypt.check_password_hash(self.password_hash, pwd)
+        
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    order_date = db.Column(db.DateTime, default=datetime.utcnow)
+    sub_total_price = db.Column(db.Float)
+    total_price = db.Column(db.Float)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
+    order_items = db.relationship(
+        'OrderItem')
