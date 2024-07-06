3e65f650c6db49102c31366023d1a36fee60981a
maureen <maureen.murimi@moringaschool.com>
2024-07-06 14:39:39 +0300

    Run migrations

--- configuration.py
+++ configuration.py
@@ -11,3 +11,33 @@ from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__ , static_url_path='/static')
+
+db = SQLAlchemy()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'sqlalchemy'
+app.config['SESSION_SQLALCHEMY'] = db
+app.config['SECRET_KEY'] = 'no_key'
+app.config['JWT_SECRET_KEY'] = 'secret-key'
+
+migrate = Migrate(app, db)
+CORS(app)
+api = Api(app)
+
+
+db.init_app(app)
+jwt = JWTManager(app)
+Session(app)
+
+mash = Marshmallow(app)
+bcrypt = Bcrypt(app)
+auth = HTTPBasicAuth()
+
+with app.app_context():
+    # Check if the 'foods' table exists
+    db.create_all()
+    inspector = inspect(db.engine)
+    # print all table names
+    if 'foods' in inspector.get_table_names():
+        print(inspector.get_table_names())
